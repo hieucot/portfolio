ba68065ae6875fabad823a6cde5b611f79bb6c0d
Nguyễn Trung Hiếu <54259196+hieucot@users.noreply.github.com>
2020-11-11 08:38:40 +0700

    Add file frist commit

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+	// Menu button
+	$("#navbarResponsive a").on("click", function (event) {
+		var hash = this.hash;
+		if (hash !== "") {
+			event.preventDefault();
+
+			$("html, body").animate({
+				scrollTop: $(hash).offset().top
+			}, 400, function () {
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			});
+		}
+	});
+
+	// Work Button
+	$(".main-work__btn").click(function () {
+		var value = $(this).attr("data-image-toggle");
+		var filter = $(".main-work__filter");
+
+		// Button Active
+		var listItems = $(".main-work__btn");
+		for (let i = 0; i < listItems.length; i++) {
+			listItems[i].classList.remove("main-work__btn-active");
+		}
+		this.classList.add("main-work__btn-active");
+
+		if (value == "all") {
+			filter.show("2000");
+		}
+		else {
+			filter.not("." + value).hide("3000");
+			filter.filter("." + value).show("3000");
+		}
+	});
+
+	// Modal Work Images
+	$(document).on('click', '[data-toggle="lightbox"]', function (event) {
+		event.preventDefault();
+		$(this).ekkoLightbox();
+	});
+
+});
